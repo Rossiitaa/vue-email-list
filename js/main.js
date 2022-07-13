012efd1819e41d9d80914e9185c713bdd9392c40
Rossiitaa <rosario.scuotto@outlook.it>
2022-07-13 15:06:18 +0200

    ten random emails in the log console

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 const app = new Vue({
     el: '#app',
     data: {
+        email: [],
 
-        
     },
     methods: {
 
-
     },
+    created: function randomEmail() {
+        for (let i = 0; i < 10; i++) {
 
-
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(response => {
+            console.log(response.data.response);
+        });
+    };
+    }
 });
